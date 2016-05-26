c33edf669522f1f65b47abd21cb2a27652d4c25f
Dom Clemmer <domclemmer@doms-imac.edw2.uc.edu>
2016-05-26 12:52:59 -0400

    build sendGenericMessage function to send a FB templated message, build logic to handle recieved messages from user: TESTING

--- app.js
+++ app.js
@@ -19,68 +19,129 @@ app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
 
-    res.send('Hello World! This is the bot\'s root endpoint!');
+	res.send('Hello World! This is the bot\'s root endpoint!');
 
 });
 
 app.get('/webhook/', function (req, res) {
 
-    if (req.query['hub.verify_token'] === verify_token) {
-        res.send(req.query['hub.challenge']);
-    }
+	if (req.query['hub.verify_token'] === verify_token) {
+		res.send(req.query['hub.challenge']);
+	}
 
-    res.send('Error, wrong validation token');
+	res.send('Error, wrong validation token');
 
 });
 
 app.post('/webhook/', function (req, res) {
 
-    var messaging_events = req.body.entry[0].messaging;
+	var messaging_events = req.body.entry[0].messaging;
 
-    for (var i = 0; i < messaging_events.length; i++) {
+	for (var i = 0; i < messaging_events.length; i++) {
 
-        var event = req.body.entry[0].messaging[i];
-        var sender = event.sender.id;
+		var event = req.body.entry[0].messaging[i];
+		var sender = event.sender.id;
 
-        if (event.message && event.message.text) {
-            var text = event.message.text;
+		if (event.message && event.message.text) {
+			var text = event.message.text;
+			if (text.toLowerCase() = "hello" || "hey") {
 
-            sendTextMessage(sender, "Echo: " + text.substring(0, 200));
-        }
-    }
+				sendGenericMessage(sender);
+				continue
+			}
+		}
+		if (event.postback) {
+			text = JSON.stringify(event.postback);
+			sendTextMessage(sender, "Postback recieved:" + text, token)
+			continue
+		}
+	}
 
-    res.sendStatus(200);
+	res.sendStatus(200);
 
 });
 
 app.listen(process.env.PORT || 3000, function () {
 
-    console.log('Facebook Messenger echoing bot started on port ' + process.env.PORT + '!');
+	console.log('Facebook Messenger echoing bot started on port ' + process.env.PORT + '!');
 
 });
 
+// MODULES FOR SENDING MESSAGES
+
+function sendGenericMessage(sender) {
+	messageData = {
+		"attachment": {
+			"type": "template",
+			"payload": {
+				"template_type": "generic",
+				"elements": [{
+					"title": "First card",
+					"subtitle": "Element #1 of an hscroll",
+					"image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+					"buttons": [{
+						"type": "web_url",
+						"url": "https://www.messenger.com",
+						"title": "web url"
+					}, {
+						"type": "postback",
+						"title": "Postback",
+						"payload": "Payload for first element in a generic bubble",
+					}],
+				}, {
+					"title": "Second card",
+					"subtitle": "Element #2 of an hscroll",
+					"image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+					"buttons": [{
+						"type": "postback",
+						"title": "Postback",
+						"payload": "Payload for second element in a generic bubble",
+					}],
+				}]
+			}
+		}
+	}
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {access_token:token},
+		method: 'POST',
+		json: {
+			recipient: {id:sender},
+			message: messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error)
+		}
+	})
+}
+
+///////
+
 function sendTextMessage(sender, text) {
 
-    var messageData = {
-        text: text
-    };
+	var messageData = {
+		text: text
+	};
 
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token: token},
-        method: 'POST',
-        json: {
-            recipient: {id: sender},
-            message: messageData
-        }
-    }, function (error, response) {
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {access_token: token},
+		method: 'POST',
+		json: {
+			recipient: {id: sender},
+			message: messageData
+		}
+	}, function (error, response) {
 
-        if (error) {
-            console.log('Error sending message: ', error);
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error);
-        }
+		if (error) {
+			console.log('Error sending message: ', error);
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error);
+		}
 
-    });
+	});
 
 }
