1a20405fd64966be2da613999941441c3f1d1d5f
niquedasneak <niquedasneak4@gmail.com>
2016-08-05 15:14:06 -0400

    add helloworld react component

--- views/admin.jsx
+++ views/admin.jsx
@@ -1,1 +1,9 @@
-admin.jsx
+var React = require('react');
+
+var HelloMessage = React.createClass({
+  render: function() {
+    return <div>Hello {this.props.name}</div>;
+  }
+});
+
+module.exports = HelloMessage;
