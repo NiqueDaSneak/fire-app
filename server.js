9bad2ace5263ca67218b4c3e572ce6761dc2ddb2
niquedasneak <niquedasneak4@gmail.com>
2016-08-05 16:04:58 -0400

    testing webhook for FB Messenger

--- server.js
+++ server.js
@@ -23,13 +23,15 @@ app.get('/', function (req, res) {
 	res.send('Hello World! This is the bot\'s root endpoint!');
 });
 
+
+
 app.get('/admin', function (req, res) {
 	res.render('admin', { name: 'World' });
 });
 
 
 var verify_token = 'my_voice_is_my_password_verify_me';
-var token = "EAAPDMUuxFGcBACRaCUr9iZAVNL0FJtpnZBtJqrHU8jnGgoSdl2JbkDlQVpeBTZBsniEJZAw7Clxod0C17lb6bGNl6ssjQNp3sj4iuZBLFMeH5JhJbPe5Q8flLpLNx9FLZAzBCjS1xTiSd6WEkVgrN54eUpmfnTR0JpnE55wgppJgZDZD";
+var token = "EAAPDMUuxFGcBAFkMpPAnuHYAdJHSiptRCnFCYMlJQQFNZCLGhwupktFPHtkSjCPr3NexjFYZALNgPFzsUnEF35L06xHbYHPzgtZBirJm8ZAu0GKgh2OpvZBRlDeORfET6RussZApD5E96VEm7sN09Hsg4PFTCy5S6OZBFVGL8DKnAZDZD";
 
 app.get('/webhook/', function (req, res) {
 	if (req.query['hub.verify_token'] === verify_token) {
