eccbceea019299c7677c775b8b4939f768bc7feb
niquedasneak <niquedasneak4@gmail.com>
2016-08-07 09:53:18 -0400

    add material UI testing table component

--- views/admin.jsx
+++ views/admin.jsx
@@ -1,6 +1,285 @@
-var React = require('react');
+// var React = require('react');
+var JsonDB = require('node-json-db');
+var db = require('./db.json');
 
-var adminApp, videoList, recentList, video, addVideoForm, editVideoForm;
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+var adminApp, videoList, videoBlock, addVideoForm, editVideoForm;
+
+
+
+adminApp = React.createClass({
+	var videos = db.getData("/");
+	render: function() {
+		return {
+			<videoList>
+			// psuedocode
+			// iterate through db
+			// draw a new video component
+			// each new video renders a li
+			// videoList is a ul
+			// videoList can sort video components
+			</videoList>
+			<addVideoForm></addVideoForm>
+		}
+	}
+});
+
+
+
+
+
+
+
+import React from 'react';
+import {Table, TableBody, TableFooter, TableHeader, TableHeaderColumn, TableRow, TableRowColumn}
+  from 'material-ui/Table';
+import TextField from 'material-ui/TextField';
+import Toggle from 'material-ui/Toggle';
+
+const styles = {
+  propContainer: {
+    width: 200,
+    overflow: 'hidden',
+    margin: '20px auto 0',
+  },
+  propToggleHeader: {
+    margin: '20px auto 10px',
+  },
+};
+
+const tableData = db;
+
+export default class TableExampleComplex extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      fixedHeader: true,
+      fixedFooter: true,
+      stripedRows: false,
+      showRowHover: false,
+      selectable: true,
+      multiSelectable: false,
+      enableSelectAll: false,
+      deselectOnClickaway: true,
+      showCheckboxes: true,
+      height: '300px',
+    };
+  }
+
+  handleToggle = (event, toggled) => {
+    this.setState({
+      [event.target.name]: toggled,
+    });
+  };
+
+  handleChange = (event) => {
+    this.setState({height: event.target.value});
+  };
+
+  render() {
+    return (
+      <div>
+        <Table
+          height={this.state.height}
+          fixedHeader={this.state.fixedHeader}
+          fixedFooter={this.state.fixedFooter}
+          selectable={this.state.selectable}
+          multiSelectable={this.state.multiSelectable}
+        >
+          <TableHeader
+            displaySelectAll={this.state.showCheckboxes}
+            adjustForCheckbox={this.state.showCheckboxes}
+            enableSelectAll={this.state.enableSelectAll}
+          >
+            <TableRow>
+              <TableHeaderColumn colSpan="3" tooltip="Super Header" style={{textAlign: 'center'}}>
+                Super Header
+              </TableHeaderColumn>
+            </TableRow>
+            <TableRow>
+              <TableHeaderColumn tooltip="The ID">ID</TableHeaderColumn>
+              <TableHeaderColumn tooltip="The Name">Name</TableHeaderColumn>
+              <TableHeaderColumn tooltip="The Status">Status</TableHeaderColumn>
+            </TableRow>
+          </TableHeader>
+          <TableBody
+            displayRowCheckbox={this.state.showCheckboxes}
+            deselectOnClickaway={this.state.deselectOnClickaway}
+            showRowHover={this.state.showRowHover}
+            stripedRows={this.state.stripedRows}
+          >
+            {tableData.map( (row, index) => (
+              <TableRow key={index} selected={row.selected}>
+                <TableRowColumn>{index}</TableRowColumn>
+                <TableRowColumn>{row.name}</TableRowColumn>
+                <TableRowColumn>{row.status}</TableRowColumn>
+              </TableRow>
+              ))}
+          </TableBody>
+          <TableFooter
+            adjustForCheckbox={this.state.showCheckboxes}
+          >
+            <TableRow>
+              <TableRowColumn>ID</TableRowColumn>
+              <TableRowColumn>Name</TableRowColumn>
+              <TableRowColumn>Status</TableRowColumn>
+            </TableRow>
+            <TableRow>
+              <TableRowColumn colSpan="3" style={{textAlign: 'center'}}>
+                Super Footer
+              </TableRowColumn>
+            </TableRow>
+          </TableFooter>
+        </Table>
+
+        <div style={styles.propContainer}>
+          <h3>Table Properties</h3>
+          <TextField
+            floatingLabelText="Table Body Height"
+            defaultValue={this.state.height}
+            onChange={this.handleChange}
+          />
+          <Toggle
+            name="fixedHeader"
+            label="Fixed Header"
+            onToggle={this.handleToggle}
+            defaultToggled={this.state.fixedHeader}
+          />
+          <Toggle
+            name="fixedFooter"
+            label="Fixed Footer"
+            onToggle={this.handleToggle}
+            defaultToggled={this.state.fixedFooter}
+          />
+          <Toggle
+            name="selectable"
+            label="Selectable"
+            onToggle={this.handleToggle}
+            defaultToggled={this.state.selectable}
+          />
+          <Toggle
+            name="multiSelectable"
+            label="Multi-Selectable"
+            onToggle={this.handleToggle}
+            defaultToggled={this.state.multiSelectable}
+          />
+          <Toggle
+            name="enableSelectAll"
+            label="Enable Select All"
+            onToggle={this.handleToggle}
+            defaultToggled={this.state.enableSelectAll}
+          />
+          <h3 style={styles.propToggleHeader}>TableBody Properties</h3>
+          <Toggle
+            name="deselectOnClickaway"
+            label="Deselect On Clickaway"
+            onToggle={this.handleToggle}
+            defaultToggled={this.state.deselectOnClickaway}
+          />
+          <Toggle
+            name="stripedRows"
+            label="Stripe Rows"
+            onToggle={this.handleToggle}
+            defaultToggled={this.state.stripedRows}
+          />
+          <Toggle
+            name="showRowHover"
+            label="Show Row Hover"
+            onToggle={this.handleToggle}
+            defaultToggled={this.state.showRowHover}
+          />
+          <h3 style={styles.propToggleHeader}>Multiple Properties</h3>
+          <Toggle
+            name="showCheckboxes"
+            label="Show Checkboxes"
+            onToggle={this.handleToggle}
+            defaultToggled={this.state.showCheckboxes}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+
+
+
+
+
+
+// videoList = React.createClass({
+// 	getInitialState() {
+// 		return {
+// 			this.state = {
+// 				sortedTrait: viewCount,
+// 				renderedList: [this.props.children]
+// 			}		
+// 		}
+// 	}
+
+// 	// function sort(trait) {
+// 	// 	this.state.sortedTrait = trait;
+
+// 	// 	if (this.state.sortedTrait === viewCount) {
+// 	// 		var newRender;
+			
+
+// 	// 		}
+// 	// 	}
+
+// 	// 	if (this.state.sortedTrait === mostRecent) {
+
+// 	// 	}
+// 	// }
+
+// 	render: function() {
+// 		return {
+// 			<div>
+// 			<p>Sort By:</p>
+// 			<input type="submit" onClick={this.sort(viewCount)}>View Count</input>
+// 			<input type="submit" onClick={this.sort(mostRecent)}>Most Recent</input>
+// 			<input type="submit" onClick={this.sort(mostRecent)}>Category</input>
+// 			<ul>
+// 				{this.state.renderedList}
+// 			</ul>
+
+// 			</div>	
+// 		}
+// 	}
+// });
+
+
+var array = [
+{ id: "Ten", value: 10},
+{ id: "Nine", value: 9},
+{ id: "Eight", value: 8},
+{ id: "Seven", value: 7},
+{ id: "Six", value: 6},
+{ id: "Five", value: 5},
+]
+
+
+
+// videoBlock = React.createClass({
+// 	// should contain editVideoForm
+// 	render: function() {
+// 		return {
+// 			<div>
+// 			<p>
+// 			{this.props.something}
+// 			</p>
+// 			</div>
+// 		}
+// 	}
+// });
 
 
 module.exports = adminApp;
