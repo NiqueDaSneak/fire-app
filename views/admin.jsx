279ac35564d879617b50c539f8518373a680efd9
niquedasneak <niquedasneak4@gmail.com>
2016-08-08 11:20:17 -0400

    build pug views that render in server.js

--- views/admin.jsx
+++ views/admin.jsx
@@ -1,3 +1,4 @@
+"use strict"
 var React = require('react');
 var db = require('diskdb');
 db.connect('db', ['videos']);
@@ -8,28 +9,59 @@ var adminApp = React.createClass({
 			<div>
 				<inputForm/>
 			</div>
-			)
+			);
 	}
 });
 
 var inputForm = React.createClass({
 	getInitialState: function() {
 		return {
+			videoURL: "",
+			videoTitle: "",
+			videoDescription: "",
+			category: "",
 		};
 	},
 	
-	submitVideo(event) {
-		event.preventDefault();
+	submitVideo: function(event) {
 		var newVideo = {
-			url: this.refs.videoURL.value,
-			youtubeID: extractYoutubeID(this.refs.videoURL.value),
-			videoTitle: this.refs.videoTitle,
-			videoDescription: this.refs.videoDescription,
+			id: makeID(),
+			url: this.state.videoURL,
+			youtubeID: extractYoutubeID(this.state.videoURL),
+			videoTitle: this.state.videoTitle,
+			videoDescription: this.state.videoDescription,
+			category: this.state.category
 		}
-		db.videos.save(newVideo)
+		db.videos.save(newVideo);
 	},
 
-	extractYoutubeID(link) { 
+	makeID: function() {
+		function s4() {
+		return Math.floor((1 + Math.random()) * 0x10000)
+		  .toString(16)
+		  .substring(1);
+		}
+		return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+		s4() + '-' + s4() + s4() + s4();
+},
+
+
+	handleInputChange: function(event) {
+		if (event.target.name === videoURL) {
+			this.setState({videoURL: event.target.value});
+		}
+		if (event.target.name === videoTitle) {
+			this.setState({videoURL: event.target.value});
+		}
+		if (event.target.name === videoDescription) {
+			this.setState({videoTitle: event.target.value});
+		}
+		if (event.target.name === category) {
+			this.setState({videoDescription: event.target.value});
+		}
+	},
+
+	extractYoutubeID: function(link) { 
 		if( link.match(/(youtube.com)/) ){
 		    var split_c = "v=";
 		    var split_n = 1;
@@ -56,22 +88,22 @@ var inputForm = React.createClass({
 	render: function() {
 		return (
 			<div>
-				<form id="videoSubmitForm" onSubmit={this.submitVideo}>
+				<form id="videoSubmitForm" onSubmit={this.submitVideo} method="post" action="/admin">
 					<label>Past Link Here: </label>
-					<input type="text" refs="videoURL"/>
+					<input type="text" name="videoURL" onChange={this.handleInputChange}/>
 					<label>Video Title: </label>
-					<input type="text" refs="videoTitle"/>
+					<input type="text" name="videoTitle" onChange={this.handleInputChange}/>
 					<label>Video Description: </label>
-					<input type="text" refs="videoDescription"/>
+					<input type="text" name="videoDescription" onChange={this.handleInputChange}/>
 					<label>Choose a Category:</label>
-					<select refs="category" form="videoSubmitForm">
+					<select name="category" form="videoSubmitForm" onChange={this.handleInputChange}>
 					</select>
 					<label>or add a new one: </label>
-					<input type="text" refs="category"/>
-					<input type="submit" value="Create New Video" onClick={this.submitVideo} />
+					<input type="text" name="category" onChange={this.handleInputChange}/>
+					<input type="submit" value="Create New Video" onClick={this.submitVideo}/>
 				</form>
 			</div>
-			)
+			);
 	}
 });
 
